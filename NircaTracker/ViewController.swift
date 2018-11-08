bb2ae788ac7bf18d84af3ea89f66f14bace5b0fb
Ethan Febinger <ethanfebinger@nbp-115-187.nbp.ruw.rutgers.edu>
2018-11-08 01:42:39 -0500

    Initial Commit

---
+++ NircaTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NircaTracker
+//
+//  Created by Ethan Febinger on 11/8/18.
+//  Copyright © 2018 Ethan Febinger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
