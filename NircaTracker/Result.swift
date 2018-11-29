5e866b0f22e924fe3f4ae4bb3d711c550348e4c7
Ethan Febinger <ethanfebinger@nbp-237-251.nbp.ruw.rutgers.edu>
2018-11-29 14:29:00 -0500

    commit

---
+++ NircaTracker/Result.swift
@@ -0,0 +1,24 @@
+//
+//  Result.swift
+//  NircaTracker
+//
+//  Created by Ethan Febinger on 11/8/18.
+//  Copyright © 2018 Ethan Febinger. All rights reserved.
+//
+
+import UIKit
+
+class Result {
+    
+    //MARK: Properties
+    
+    var meetName : String
+    var time : String
+    
+    init(meetName: String, time: String) {
+        self.meetName = meetName
+        self.time = time
+    }
+    
+}
+
